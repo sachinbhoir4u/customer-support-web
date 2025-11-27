f624e3166da6d32c577e558a7e3d4a2942cfa580
sachinbhoir4u <sachinbhoir4u@gmail.com>
2025-11-27 19:41:08 +0530

    Avatar component added

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
         ghost:
           'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
-        solid: 'bg-black text-white text-shadow-2xs hover:text-shadow-white focus-visible:ring-gray-200',
+        solid: 'bg-black text-white text-shadow-2xs hover:text-shadow-white focus-visible:ring-gray-200 cursor-pointer',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
