e3863420ef5d17dfb9020981c7be903b6d83a752
sachinbhoir4u <sachinbhoir4u@gmail.com>
2025-11-28 21:37:03 +0530

    Avatar- minor css fix

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -13,7 +13,7 @@ function Avatar({
     <AvatarPrimitive.Root
       data-slot="avatar"
       className={cn(
-        'relative flex size-8 shrink-0 overflow-hidden rounded-full',
+        'relative flex size-8 shrink-0 overflow-hidden rounded-full border border-gray-300 bg-gray-100',
         className,
       )}
       {...props}
